0dc407e99092e334e165c1c1b328cf306f1b882e
tas09009 <taniya.singh12@gmail.com>
2021-09-07 16:54:22 -0400

    black + flake8 format

--- app/api/books.py
+++ app/api/books.py
@@ -1,69 +1,70 @@
 from urllib.request import urlopen
 from urllib.parse import urlencode
 import re
-import requests
 
 from flask import request, redirect, url_for
-import flask_excel as excel
-import xmltodict, json
+import flask_excel as excel  # not being used?
+import xmltodict
 
-from app.models import Book, User, TenCategories, HundredCategories, ThousandCategories
+from app.models import (
+    Book,
+    User,
+    TenCategories,
+    HundredCategories,
+    ThousandCategories,
+)
 from . import api
 from .. import db
-from .decorators import permission_required
-
 
 
 # Add ['DELETE'] method. Doesn't remove book info, only user association
 
 
 # http://127.0.0.1:5000/api/v1/books/upload
-@api.route('/books/upload', methods=['GET', 'POST'])
-# @permission_required(permission.WRITE)
+@api.route("/books/upload", methods=["GET", "POST"])
 def csv_import():
-    if request.method == 'POST':
+    if request.method == "POST":
 
         def book_init_func(row):
-            book_instance = Book(row['title'])
-            book_instance.author = row['author']
-            book_instance.isbn = row['isbn']
-            book_instance.isbn13 = row['isbn13']
-               
-            '''Cleanup ISBN numbers'''        
+            book_instance = Book(row["title"])
+            book_instance.author = row["author"]
+            book_instance.isbn = row["isbn"]
+            book_instance.isbn13 = row["isbn13"]
+
+            """Cleanup ISBN numbers"""
             # book_instance.isbn = clean_isbn(book_instance.isbn)
             # book_instance.isbn13 = clean_isbn(book_instance.isbn13)
 
-            '''Add rows to User.books'''        
-            user = User.query.filter_by(username='john').first()
+            """Add rows to User.books"""
+            user = User.query.filter_by(username="john").first()
             user.books.append(book_instance)
-        
+
             return book_instance
-                     
+
         mapdict = {
-            'Title' : 'title',
-            'Author' : 'author',
-            'ISBN' : 'isbn',
-            'ISBN13' : 'isbn13',
-            }
+            "Title": "title",
+            "Author": "author",
+            "ISBN": "isbn",
+            "ISBN13": "isbn13",
+        }
 
         request.isave_to_database(
             field_name="file",
             session=db.session,
             table=Book,
             initializer=book_init_func,
-            mapdict=mapdict
-        )      
+            mapdict=mapdict,
+        )
 
-
-        '''Create Ten and Hundred nested relationship'''
+        """Create Ten and Hundred nested relationship"""
         ten_and_hundred()
 
+        """Dewey Number + Category"""
+        dewey_and_thousand()
 
-        '''Dewey Number + Category'''
-        dewey_and_thousand()  
-
-
-        return redirect(url_for("main.view_books_by_user", username='john'), code=302) 
+        return redirect(
+            url_for("main.view_books_by_user", username="john"), code=302
+        )
 
     return """
     <!doctype html>
@@ -75,43 +76,53 @@ def csv_import():
     """
 
 
-
 def dewey_and_thousand():
-    user = User.query.filter_by(username='john').first()
+    user = User.query.filter_by(username="john").first()
     books = user.books.all()
-    for book_instance in books:      
-        book_instance.classify_ddc = request_book_data(book_instance.isbn) 
+    for book_instance in books:
+        book_instance.classify_ddc = request_book_data(book_instance.isbn)
 
         db.session.add(book_instance)
         db.session.commit()
 
-        book_instance.classify_ten_id = dewey_to_category_ten(book_instance.classify_ddc) 
-        book_instance.classify_hundred_id = dewey_to_category_hundred(book_instance.classify_ddc)
-        book_instance.classify_thousand_id = dewey_to_category_thousand(book_instance.classify_ddc)          
-        
+        book_instance.classify_ten_id = dewey_to_category_ten(
+            book_instance.classify_ddc
+        )
+        book_instance.classify_hundred_id = dewey_to_category_hundred(
+            book_instance.classify_ddc
+        )
+        book_instance.classify_thousand_id = dewey_to_category_thousand(
+            book_instance.classify_ddc
+        )
+
         if book_instance.classify_ten_id:
-            category_obj = TenCategories.query.filter_by(id=book_instance.classify_ten_id).first()
+            category_obj = TenCategories.query.filter_by(
+                id=book_instance.classify_ten_id
+            ).first()
             category_obj.books.append(book_instance)
             db.session.add(category_obj)
             db.session.commit()
         if book_instance.classify_hundred_id:
-            category_obj = HundredCategories.query.filter_by(id=book_instance.classify_hundred_id).first()
+            category_obj = HundredCategories.query.filter_by(
+                id=book_instance.classify_hundred_id
+            ).first()
             category_obj.books.append(book_instance)
             db.session.add(category_obj)
             db.session.commit()
         if book_instance.classify_thousand_id:
-            category_obj = ThousandCategories.query.filter_by(id=book_instance.classify_thousand_id).first()
+            category_obj = ThousandCategories.query.filter_by(
+                id=book_instance.classify_thousand_id
+            ).first()
             category_obj.books.append(book_instance)
             db.session.add(category_obj)
             db.session.commit()
 
-        db.session.add(book_instance) # ?? Redundant with add + commit above?
+        db.session.add(book_instance)  # ?? Redundant with add + commit above?
         db.session.commit()
     return
 
 
 def ten_and_hundred():
-
     def populate_ten_classes():
         ten_cat = TenCategories.query.all()
         hun_cat = HundredCategories.query.all()
@@ -126,7 +137,6 @@ def ten_and_hundred():
             stop += 10
         db.session.commit()
         return
-
 
     def populate_hundred_classes():
         hun_cat = HundredCategories.query.all()
@@ -143,69 +153,93 @@ def ten_and_hundred():
         db.session.commit()
         return
 
-        
     populate_ten_classes()
     populate_hundred_classes()
 
+
 # isbn to Dewey decimal
-@api.route('/dewey/', methods=["GET"])
+@api.route("/dewey/", methods=["GET"])
 def request_book_data(isbn):
-    '''Classify API from ISBN -> JSON of book'''
+    """Classify API from ISBN -> JSON of book"""
 
     if isbn:
-      
-        base = 'http://classify.oclc.org/classify2/Classify?'
-        summaryBase = '&summary=true'
-        parm_type = 'isbn'
+
+        base = "http://classify.oclc.org/classify2/Classify?"
+        summaryBase = "&summary=true"
+        parm_type = "isbn"
         parm_value = isbn
-        search_url = base + urlencode({parm_type:parm_value.encode('utf-8')}) + summaryBase
-        xml_content = urlopen(search_url).read() #TODO use requests library instead. Also use response codes
-        xmlDict = xmltodict.parse(xml_content)          
-        
+        search_url = (
+            base
+            + urlencode({parm_type: parm_value.encode("utf-8")})
+            + summaryBase
+        )
+        xml_content = urlopen(
+            search_url
+        ).read()  # TODO use requests library instead. Also use response codes
+        xmlDict = xmltodict.parse(xml_content)
+
         isbnDirect = isbn_to_dewey(xmlDict)
         return isbnDirect
 
     elif isbn is None:
-        return 'No ISBN provided'
+        return "No ISBN provided"
 
 
 def isbn_to_dewey(xmlDict):
 
-    if xmlDict.get("classify").get('response').get('@code') == "0":
+    if xmlDict.get("classify").get("response").get("@code") == "0":
         try:
-            dewey_number = xmlDict.get("classify").get('recommendations').get('ddc').get('mostPopular').get('@sfa')
+            dewey_number = (
+                xmlDict.get("classify")
+                .get("recommendations")
+                .get("ddc")
+                .get("mostPopular")
+                .get("@sfa")
+            )
             return dewey_number
         except AttributeError:
             return "Returned book response, but has no dewey number"
-    elif xmlDict.get("classify").get('response').get('@code') == "4":
+    
+    elif xmlDict.get("classify").get("response").get("@code") == "4":
         try:
-            owi_number = xmlDict.get("classify").get('works').get('work')[0].get('@owi')
+            owi_number = (
+                xmlDict.get("classify").get("works").get("work")[0].get("@owi")
+            )
 
-            base = 'http://classify.oclc.org/classify2/Classify?'
-            parm_type = 'owi'
+            base = "http://classify.oclc.org/classify2/Classify?"
+            parm_type = "owi"
             parm_value = owi_number
-            search_url = base + urlencode({parm_type:parm_value.encode('utf-8')})
-            xml_content = urlopen(search_url).read() 
-            xmlDict = xmltodict.parse(xml_content)  
+            search_url = base + urlencode(
+                {parm_type: parm_value.encode("utf-8")}
+            )
+            xml_content = urlopen(search_url).read()
+            xmlDict = xmltodict.parse(xml_content)
 
-            dewey_number = xmlDict.get("classify").get('recommendations').get('ddc').get('mostPopular').get('@sfa')
+            dewey_number = (
+                xmlDict.get("classify")
+                .get("recommendations")
+                .get("ddc")
+                .get("mostPopular")
+                .get("@sfa")
+            )
             return dewey_number
         except AttributeError:
             return "multiple works found, use OWI"
     else:
-        return 'Key Error'
+        return "Key Error"
 
 
+"""Find the corresponding Category titles"""
 
 
-'''Find the corresponding Category titles'''
 def dewey_to_category_ten(dewey_number):
     ten_cat = TenCategories.query.all()
-    
+
     firstNum_ten = dewey_number[0]
     for category in ten_cat:
         if firstNum_ten == category.call_number[0]:
             return category.id
+
 
 def dewey_to_category_hundred(dewey_number):
     hun_cat = HundredCategories.query.all()
@@ -214,6 +248,7 @@ def dewey_to_category_hundred(dewey_number):
     for category in hun_cat:
         if firstNum_hundred == category.call_number[0:2]:
             return category.id
+
 
 def dewey_to_category_thousand(dewey_number):
     thou_cat = ThousandCategories.query.all()
@@ -226,42 +261,42 @@ def dewey_to_category_thousand(dewey_number):
 
 def clean_isbn(isbn):
     filter_isbn = re.findall("[a-zA-Z0-9]", isbn)
-    join_isbn = ('').join(filter_isbn)
+    join_isbn = ("").join(filter_isbn)
     return join_isbn
-
-
 
 
 # -----------------------------------------------------------------------------------------------
 # One time upload of categories, Admin only
 
-@api.route('/category/ten/upload', methods=['GET', 'POST'])
+
+@api.route("/category/ten/upload", methods=["GET", "POST"])
 def csv_import_ten_categories():
-    if request.method == 'POST':
+    if request.method == "POST":
 
         def category_init_func(row):
             category_instance = TenCategories()
-            category_instance.call_number = row['call_number']
-            category_instance.classification = row['classification']
+            category_instance.call_number = row["call_number"]
+            category_instance.classification = row["classification"]
 
-            # category_instance = TenCategories_DDC(row['call_number'], row['classification'])
             return category_instance
 
-                     
         mapdict = {
-            'Call Number' : 'call_number',
-            'Classification' : 'classification',
-            }
+            "Call Number": "call_number",
+            "Classification": "classification",
+        }
 
         request.isave_to_database(
             field_name="file",
             session=db.session,
             table=TenCategories,
             initializer=category_init_func,
-            mapdict=mapdict
-        )      
-          
-        return redirect(url_for("main.view_books_ten_categories", username='john'), code=302)
+            mapdict=mapdict,
+        )
+
+        return redirect(
+            url_for("main.view_books_ten_categories", username="john"),
+            code=302,
+        )
 
     return """
     <!doctype html>
@@ -273,33 +308,34 @@ def csv_import_ten_categories():
     """
 
 
-@api.route('/category/hundred/upload', methods=['GET', 'POST'])
+@api.route("/category/hundred/upload", methods=["GET", "POST"])
 def csv_import_hundred_categories():
-    if request.method == 'POST':
+    if request.method == "POST":
 
         def category_init_func(row):
             category_instance = HundredCategories()
-            category_instance.call_number = row['call_number']
-            category_instance.classification = row['classification']
+            category_instance.call_number = row["call_number"]
+            category_instance.classification = row["classification"]
 
-            # category_instance = HundredCategories_DDC(row['call_number'], row['classification'])
             return category_instance
 
-                     
         mapdict = {
-            'Call Number' : 'call_number',
-            'Classification' : 'classification',
-            }
+            "Call Number": "call_number",
+            "Classification": "classification",
+        }
 
         request.isave_to_database(
             field_name="file",
             session=db.session,
             table=HundredCategories,
             initializer=category_init_func,
-            mapdict=mapdict
-        )      
-          
-        return redirect(url_for("main.view_books_hundred_categories", username='john'), code=302)
+            mapdict=mapdict,
+        )
+
+        return redirect(
+            url_for("main.view_books_hundred_categories", username="john"),
+            code=302,
+        )
 
     return """
     <!doctype html>
@@ -311,33 +347,34 @@ def csv_import_hundred_categories():
     """
 
 
-@api.route('/category/thousand/upload', methods=['GET', 'POST'])
+@api.route("/category/thousand/upload", methods=["GET", "POST"])
 def csv_import_thousand_categories():
-    if request.method == 'POST':
+    if request.method == "POST":
 
         def category_init_func(row):
             category_instance = ThousandCategories()
-            category_instance.call_number = row['call_number']
-            category_instance.classification = row['classification']
+            category_instance.call_number = row["call_number"]
+            category_instance.classification = row["classification"]
 
-            # category_instance = ThousandCategories_DDC(row['call_number'], row['classification'])
             return category_instance
 
-                     
         mapdict = {
-            'Call Number' : 'call_number',
-            'Classification' : 'classification',
-            }
+            "Call Number": "call_number",
+            "Classification": "classification",
+        }
 
         request.isave_to_database(
             field_name="file",
             session=db.session,
             table=ThousandCategories,
             initializer=category_init_func,
-            mapdict=mapdict
-        )      
-          
-        return redirect(url_for("main.view_books_thousand_categories", username='john'), code=302)
+            mapdict=mapdict,
+        )
+
+        return redirect(
+            url_for("main.view_books_thousand_categories", username="john"),
+            code=302,
+        )
 
     return """
     <!doctype html>
@@ -347,4 +384,3 @@ def csv_import_thousand_categories():
     <input type=file name=file><input type=submit value=Upload>
     </form>
     """
-
--- app/main/views.py
+++ app/main/views.py
@@ -1,53 +1,48 @@
-from flask import render_template, jsonify, request, url_for, redirect
+from flask import render_template, jsonify
 from flask_login import login_required, current_user
 from . import main
-from .. import db
-from app.models import Book, User, TenCategories, HundredCategories, ThousandCategories
+from app.models import (
+    User,
+    TenCategories,
+    HundredCategories,
+    ThousandCategories,
+)
 
 
-
-@main.route('/')
+@main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@main.route('/circlepacking') #TODO user isn't used in function
+
+@main.route("/books_by_category")  # TODO user isn't used in function
 @login_required
 def circle_packing_view():
 
     # user = User.query.filter_by(id=current_user.id).first()
     user_books = User.query.filter_by(id=current_user.id).first().books.all()
 
-    '''Each of their respective objects'''
     ten_cat = TenCategories.query.all()
-    hun_cat = HundredCategories.query.all()
-    thou_cat = ThousandCategories.query.all()
 
-    '''Each of their respective classifications'''
-    ten_cat_list = [ten_category.classification for ten_category in ten_cat]
-    hun_cat_list = [hun_category.classification for hun_category in hun_cat]
-    thou_cat_list = [thou_category.classification for thou_category in thou_cat]
-
-    
-    
     tens_list = []
-    books_dict = {"name" : "books", "children" : tens_list}
+    books_dict = {"name": "books", "children": tens_list}
     for i in ten_cat:
-        ten_placeholder = {} # dict of tens category
+        ten_placeholder = {}
         hun_list = []
-        ten_title = i.call_number + ' | ' + i.classification # string tens list
+        ten_title = (
+            i.call_number + " | " + i.classification
+        )  
         ten_placeholder["name"] = ten_title
         ten_placeholder["children"] = hun_list
         for j in i.hundred_values:
             hun_placeholder = {}
             tho_list = []
-            hun_title = j.call_number + ' | ' + j.classification
+            hun_title = j.call_number + " | " + j.classification
             hun_placeholder["name"] = hun_title
             hun_placeholder["children"] = tho_list
             hun_list.append(hun_placeholder)
             for k in j.thousand_values:
                 tho_placeholder = {}
-                book_list = []
-                tho_title = k.call_number + ' | ' + k.classification
+                tho_title = k.call_number + " | " + k.classification
                 tho_placeholder["name"] = tho_title
                 tho_placeholder["children"] = [i.title for i in k.books]
 
@@ -55,26 +50,25 @@ def circle_packing_view():
 
                 # tho_placeholder["children"] = []
                 # for book in user_books:
-                #     if book.classify_thousand_id == 
-
+                #     if book.classify_thousand_id ==
 
                 tho_list.append(tho_placeholder)
 
         tens_list.append(ten_placeholder)
-  
+
     return jsonify(books_dict)
 
 
-@main.route('/bookshelf')
+@main.route("/list_of_books")
 @login_required
 def view_books_by_user():
-
     user_books = User.query.filter_by(id=current_user.id).first().books.all()
 
     books_list = [book.serialize() for book in user_books]
     return jsonify(books_list)
 
-@main.route('/ten_categories')
+
+@main.route("/ten_categories")
 @login_required
 def view_books_ten_categories():
     user_books = User.query.filter_by(id=current_user.id).first().books.all()
@@ -85,13 +79,13 @@ def view_books_ten_categories():
         classification = category.to_json()
         for book in user_books:
             if book.classify_ten_id == category.id:
-                classification['books'].append(book.title)
+                classification["books"].append(book.title)
 
         users_books_within_ten.append(classification)
     return jsonify(users_books_within_ten)
 
 
-@main.route('/hundred_categories')
+@main.route("/hundred_categories")
 @login_required
 def view_books_hundred_categories():
 
@@ -103,14 +97,13 @@ def view_books_hundred_categories():
         classification = category.to_json()
         for book in user_books:
             if book.classify_ten_id == category.id:
-                classification['books'].append(book.title)
+                classification["books"].append(book.title)
 
         users_books_within_ten.append(classification)
     return jsonify(users_books_within_ten)
 
 
-
-@main.route('/thousand_categories')
+@main.route("/thousand_categories")
 @login_required
 def view_books_thousand_categories():
     user_books = User.query.filter_by(id=current_user.id).first().books.all()
@@ -121,9 +114,7 @@ def view_books_thousand_categories():
         classification = category.to_json()
         for book in user_books:
             if book.classify_ten_id == category.id:
-                classification['books'].append(book.title)
+                classification["books"].append(book.title)
 
         users_books_within_ten.append(classification)
     return jsonify(users_books_within_ten)
-
-
