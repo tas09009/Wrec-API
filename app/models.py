0c16bc290552af492e2f741274a7e0d78069e7fb
tas09009 <taniya.singh12@gmail.com>
2021-10-05 19:10:17 -0400

    registration + email setup

--- app/models.py
+++ app/models.py
@@ -33,6 +33,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(128))
+    # confirmed = db.Column(db.Boolean, default=False)
     books = db.relationship('Book',
                             secondary=bookshelf,
                             backref=db.backref('users', lazy='dynamic'),
@@ -49,6 +50,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def generate_auth_token(self, expiration):
         s = Serializer(current_app.config['SECRET_KEY'],
