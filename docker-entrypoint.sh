26d8cf809fe9632c7a32b9cee50c821979af1ac8
tas09009 <taniya.singh12@gmail.com>
2023-07-19 15:14:43 -0400

    increase timeout 120 secs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,5 @@
 # Container will run migrations before app starts.
 flask db upgrade
 
-exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
+# Start the server with a timeout of 120 seconds
+exec gunicorn --bind 0.0.0.0:80 --timeout 120 "app:create_app()"
