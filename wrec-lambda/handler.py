15aa763456edaf9072a26b9156469991a148b2e3
Taniya <taniya.singh12@gmail.com>
2024-02-09 17:52:35 -0500

    nit updates to handler.py

--- wrec-lambda/handler.py
+++ wrec-lambda/handler.py
@@ -1,51 +1,100 @@
-import json
+import os
+from io import StringIO
+import re
+import unicodedata
+import chardet
+import csv
 import boto3
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+import sqlalchemy as sa
 
-test_db = "postgresql+psycopg2://seupapiy:4Ki4A2gKXs70TMb8xq_fXrJq8aJGmfGz@batyr.db.elephantsql.com/seupapiy"
-db_engine = create_engine(test_db)
 
-def hello(event, context):
+def handler(event, context):
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise ValueError("DATABASE_URL environment variable not set")
+    db_engine = create_engine(db_url)
 
-    # TEST: DB connection
     with db_engine.connect() as connection:
-        print(connection)
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        file_key = event["Records"][0]["s3"]["object"]["key"]
 
-        query = text("SELECT * FROM books WHERE title=:title")
-        result = connection.execute(query, {'title': 'Kafka on the Shore'})
-        print(f'result: {result}')
+        s3_client = boto3.client("s3")  # stay in the function?
+        file_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+        file_content = file_obj["Body"].read()
 
-        books = result.fetchall()
-        print(f'books: {books}')
+        # Check for the encoding of the CSV content
+        # Use the detected encoding to decode the content
+        charset = chardet.detect(file_content)["encoding"]
+        dataFile = StringIO(file_content.decode(charset))
+        csv_reader = csv.reader(dataFile)
+        book_data = parse_csv_data(csv_reader)
+        process_book_data(book_data, connection)
 
-        # Add assertions based on what you expect to find in the database
-        # print(f'books: {books}')
-        assert len(books) > 0  # Example assertion
-
-
-    body = {
-        # "books": books,
-        "input": event,
-    }
-
-    # TEST: S3 bucket
-    bucket_name = "wrec-upload-book-csv"
-    file_key = "user-csv/1/goodreads_library_export_sample.csv" #TODO: remove hardcoded path
-
-    s3_client = boto3.client('s3')
-    file_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
-
-    file_content = file_obj['Body'].read()
-    print(f'file_content: {file_content}')
+        return {"statusCode": 200, "body": "Processed books successfully"}
 
 
-    # # TEST: importing packages
-    # try:
-    #     import chardet
-    #     print(f"The package {chardet.__name__} has been successfully imported.")
-    #     import sqlalchemy
-    #     print(f"The package {sqlalchemy.__name__} has been successfully imported.")
-    # except ImportError:
-    #     print("Failed to import the package.")
+def extract_number(value):
+    # Use a regular expression to find digits in the string
+    match = re.search(r"\d+", value)
+    if match:
+        return match.group()
+    return None
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+
+def parse_csv_data(csv_reader):
+    ISBN_10_IDX = 5
+    ISBN_13_IDX = 6
+    TITLE_IDX = 1
+    AUTHOR_IDX = 2
+    parsed_data = []
+
+    # Skip the header row
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        isbn_10 = extract_number(row[ISBN_10_IDX])
+        isbn_13 = extract_number(row[ISBN_13_IDX])
+        isbn = isbn_10 or isbn_13
+        author = (
+            unicodedata.normalize("NFD", row[AUTHOR_IDX])
+            .encode("ascii", "ignore")
+            .decode("utf-8")
+        )
+        title = (
+            unicodedata.normalize("NFD", row[TITLE_IDX])
+            .encode("ascii", "ignore")
+            .decode("utf-8")
+        )
+
+        parsed_data.append({"isbn": isbn, "title": title, "author": author})
+
+    return parsed_data
+
+
+def process_book_data(book_data, connection):
+    for book in book_data:
+        result = connection.execute(
+            sa.text("SELECT * FROM books WHERE title = :title"),
+            {"title": book["title"]},
+        )
+        existing_book = result.fetchone()
+        print(f"existing book: {existing_book}")
+
+        if not existing_book:
+            insert_stmt = sa.text(
+                """
+                INSERT INTO books (isbn, title, author)
+                VALUES (:isbn, :title, :author)
+            """
+            )
+            connection.execute(
+                insert_stmt,
+                {
+                    "isbn": book["isbn"],
+                    "title": book["title"],
+                    "author": book["author"],
+                },
+            )
+            connection.commit()
+            print(f"Added book: {book['title']} | {book['author']} | {book['isbn']}")
